bbde7cb2a0bbe92b3568b2e5b80c56887d834395
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-15 07:37:28 +0300

    Update input.js

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,38 +1,38 @@
 $(document).on('ajaxComplete ready', function () {
+    // Initialize $sliders
+    $('[data-provides="anomaly.field_type.slider"]:not([data-initialized])')
+        .each(function () {
+            var $slider = $(this);
+            var value = String($slider.data('value')).split(',');
 
-    // Initialize sliders
-    $('[data-provides="anomaly.field_type.slider"]:not([data-initialized])').each(function () {
+            $slider
+                .attr('data-initialized', '')
+                .slider({
+                    min: $slider.data('min'),
+                    max: $slider.data('max'),
+                    step: $slider.data('step'),
+                    range: $slider.data('range')
+                });
 
-        var slider = $(this);
-
-        var value = String(slider.data('value')).split(',');
-
-        slider
-            .attr('data-initialized', '')
-            .slider({
-                min: slider.data('min'),
-                max: slider.data('max'),
-                step: slider.data('step'),
-                range: slider.data('range')
-            });
-
-        if (value.length > 1) {
-            slider.slider('values', value);
-        } else {
-            slider.slider('value', value);
-        }
-
-        slider.addSliderSegments(slider.data('max'));
-
-        slider.on('slide', function (event, ui) {
-
-            if (ui.values == undefined) {
-                slider.prev('.value-label').find('.value').text(ui.value);
-                slider.find('input').val(ui.value);
+            if (value.length > 1) {
+                $slider.slider('values', value);
             } else {
-                slider.prev('.value-label').find('.value').text(String(ui.values).replace(',', '-'));
-                slider.find('input').val(ui.values);
+                $slider.slider('value', value);
             }
+
+            $slider.addSliderSegments($slider.data('max'));
+
+            $slider.on('slide', function (event, ui) {
+                var $input = $slider.find('input');
+                var $value = $slider.prev('.value-label').find('.value');
+
+                if (ui.values === undefined) {
+                    $value.text(ui.value);
+                    $input.val(ui.value);
+                } else {
+                    $value.text(String(ui.values).replace(',', '-'));
+                    $input.val(ui.values);
+                }
+            });
         });
-    });
 });
